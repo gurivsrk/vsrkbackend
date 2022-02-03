41e9ee180ddc022a8fdb3ad4b45bf4c15893a3dc
Gursharan Singh <gursharan@vsrkcapital.com>
2022-02-03 17:53:20 +0530

    only 4 member on front

--- app/Http/Controllers/frontend/homeController.php
+++ app/Http/Controllers/frontend/homeController.php
@@ -19,7 +19,7 @@ class homeController extends Controller
     public function index(){
 
         $team = team::orderBy('order_id')->get()->take(3);
-        $blogs = blogs::reverse()->get()->take(3);
+        $blogs = blogs::reverse()->get()->take(4);
         $testimonials = testimonials::reverse()->get();
         $brand_logo = category::select('name','logo')->whereNotNull('logo')->get();
 
