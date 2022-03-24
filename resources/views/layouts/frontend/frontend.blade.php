f3d151118af72bb9006c372b86d8e9349f004a06
Gursharan Singh <gursharan@vsrkcapital.com>
2022-03-24 18:25:31 +0530

    remove other basic errors

--- resources/views/layouts/frontend/frontend.blade.php
+++ resources/views/layouts/frontend/frontend.blade.php
@@ -4,7 +4,7 @@
          <title>{{@$gd->website_title}}</title>
         @yield('meta_data')   
         <!-- favicon -->
-        <link rel="icon" href="{{asset($gd->favicon)}}" type="image/png" sizes="16x16" />    
+        <link rel="icon" href="{{asset(@$gd->favicon)}}" type="image/png" sizes="16x16" />    
         <!-- bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- fontawesome CSS -->
--- resources/views/layouts/page_templates/guest.blade.php
+++ resources/views/layouts/page_templates/guest.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
   <div class="container">
     <div class="navbar-wrapper">
-      <a class="navbar-brand" href="{{ route('frontend.index') }}">{{ $gd->website_title }}</a>
+      <a class="navbar-brand" href="{{ route('frontend.index') }}">{{ @$gd->website_title }}</a>
     </div>
   </div>
 </nav>
