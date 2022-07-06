759f48123e8f996f1bc4841c4bf6f1b0e51dd667
Gursharan Singh <gursharan@vsrkcapital.com>
2022-07-06 17:07:35 +0530

    bug 2 in homecontroller2

--- resources/views/partials/frontend/blog.blade.php
+++ resources/views/partials/frontend/blog.blade.php
@@ -1,34 +1,36 @@
   <!--start blog section-->
 
         <div class="row justify-content-center">
-            @foreach($blogs as $blog)
-            <div class="{{empty($class) ? 'col-lg-4' : $class}} col-md-6 col-sm-12">
-                <div class="blog-item-single">
-                    <div class="blog-item-img">
-                        <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}">
-                            <img src="{{asset($blog->blogImage)}}" alt="{{$blog->slug}}-image" />
-                        </a>
-                        <p class="tag">{{$blog->tagName[0]}}</p>
+            @if(count($blogs) !== 0)
+                @foreach($blogs as $blog)
+                <div class="{{empty($class) ? 'col-lg-4' : $class}} col-md-6 col-sm-12">
+                    <div class="blog-item-single">
+                        <div class="blog-item-img">
+                            <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}">
+                                <img src="{{asset($blog->blogImage)}}" alt="{{$blog->slug}}-image" />
+                            </a>
+                            <p class="tag">{{$blog->tagName[0]}}</p>
+                        </div>
+                        <div class="blog-item-content">
+                            <span> <i class="envy envy-calendar"></i>{{$blog->BlogDate}}</span>
+                            <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}">
+                                <h3>{!!(Str::length($blog->title) < 40 ? wordwrap($blog->title, 30 , "<br>" ,true) : $blog->title)!!}</h3>
+                            </a>
+                            @if(empty($class))
+                            <p>
+                                {!! Str::limit($blog->descritption,200) !!}
+                            </p>
+                            <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}" target="_self" class="btn btn-text-only">
+                                read more
+                                <i class="envy envy-right-arrow"></i>
+                            </a>
+                            @endif
+                        </div>
+                        <!-- blog-item-content -->
                     </div>
-                    <div class="blog-item-content">
-                        <span> <i class="envy envy-calendar"></i>{{$blog->BlogDate}}</span>
-                        <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}">
-                            <h3>{!!(Str::length($blog->title) < 40 ? wordwrap($blog->title, 30 , "<br>" ,true) : $blog->title)!!}</h3>
-                        </a>
-                        @if(empty($class))
-                        <p>
-                            {!! Str::limit($blog->descritption,200) !!}
-                        </p>
-                        <a href="{{route('frontend.blog_detail',[$blog->id,$blog->slug])}}" target="_self" class="btn btn-text-only">
-                            read more
-                            <i class="envy envy-right-arrow"></i>
-                        </a>
-                        @endif
-                    </div>
-                    <!-- blog-item-content -->
+                    <!-- blog-item-single -->
                 </div>
-                <!-- blog-item-single -->
-            </div>
-        @endforeach
+            @endforeach
+            @endif
         </div>
         <!-- row -->
